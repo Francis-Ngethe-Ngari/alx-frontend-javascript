bcb7fe81df58a782256316f326d0fbb4cc61bb66
Francis-Ngethe-Ngari <francisngari49@gmail.com>
2024-10-08 23:34:03 +0300

    debugging syntax to ensure condition block does not get executed

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
-
-  if (trueOrFalse) {
-    let task = true;
-    let task2 = false;
-  }
+  let task = false;
+  let task2 = true;
 
   return [task, task2];
+
+  if (trueOrFalse) {
+    task = true;
+    task2 = false;
+ }
 }
